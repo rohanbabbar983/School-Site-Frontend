01d680fcdda16ebf31fd384ddd7d7b5b3b4f6bcd
Rohan Babbar <rohanbabbar2003@gmail.com>
2024-07-26 00:45:40 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,20 @@ import heroVideo from "../assets/kid-with-telescope.mp4";
 
 function Hero() {
   return (
-    <div className="relative w-full h-[80vh] sm:h-[100vh] overflow-hidden">
+    <div className="relative h-[100vh] overflow-hidden">
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
         src={heroVideo}
         autoPlay
         loop
         muted
-        playsInline
       />
-      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-25" />
-      <section className="absolute bottom-8 right-4 p-4 bg-black bg-opacity-75 rounded-lg shadow-lg max-w-xs sm:max-w-md">
-        <p className="text-white text-sm md:text-xl font-extrabold font-serif">
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-15" />
+      <section className="absolute bottom-8 right-0 p-4 bg-black bg-opacity-95 rounded-l-lg shadow-lg">
+        <p className="text-white text-sm md:text-3xl font-extrabold font-serif">
           Welcome to Springdale Public School,
         </p>
-        <p className="text-white text-xs md:text-base font-light font-sans mt-2">
+        <p className="text-white text-sm md:text-xl font-light font-sans mt-2">
           where we nurture young minds for a brighter future....
         </p>
       </section>
